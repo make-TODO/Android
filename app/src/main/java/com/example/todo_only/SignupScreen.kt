0ac9c6a08647b8fea6354bbd8f4e0c833d051f78
dayeon <dana090825@gmail.com>
2025-11-03 20:01:42 +0900

    feat : signupScreen 개발ing

--- app/src/main/java/com/example/todo_only/SignupScreen.kt
+++ app/src/main/java/com/example/todo_only/SignupScreen.kt
@@ -1,9 +1,60 @@
 package com.example.todo_only
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SignupScreen(
     onSignupSuccess: () -> Unit) {
+
+    var id by rememberSaveable { mutableStateOf("") }
+    var pw by rememberSaveable { mutableStateOf("") }
+    var confirmPw by rememberSaveable { mutableStateOf("") }
+
+    Box (
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding()
+            .background(color = colorResource(R.color.bg))
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp)
+        ) {
+            Spacer(Modifier.height(49.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.todo_logo_text),
+                contentDescription = "앱 텍스트 로고",
+                modifier = Modifier
+                    .size(165.dp)
+                    .fillMaxWidth(),
+            )
+
+            Spacer(Modifier.height(124.dp))
+
+            OutlinedTextField(
+
+            )
+        }
+    }
 
 }
